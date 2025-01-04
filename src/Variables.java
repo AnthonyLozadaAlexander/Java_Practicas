b7612c54aaea7ff55f73b3818cb9be3225d6829e
zDefcon20 <antholozadarequelme@gmail.com>
2025-01-03 23:26:16 -0500

    update

---
+++ src/Variables.java
@@ -0,0 +1,23 @@
+public class Variables {
+    public static void main(String[] args) {
+
+        // Declaracion de variables
+        String nombre = "Anthony"; // Variable de tipo String
+        int edad = 20; // Variable de tipo entero
+        double estatura = 1.68; // Variable de tipo double
+        boolean esMayorEdad = true; // Variable de tipo boolean
+        char letra = 'A'; // Variable de tipo char
+        String direccion = "America Sur";
+        long numeroGrande = 1234567890; // Variable de tipo long
+
+        System.out.println("Mi nombre es: "+ nombre + " y tengo: " + edad + " años");
+        System.out.println("Mi estature es: " + estatura + " metros");
+        System.out.println("Soy de " + direccion);
+        System.out.println("La inicial de mi nombre es: " + letra);
+        System.out.println("Mi numero de telefono es: " + numeroGrande);
+        System.out.println("¿Soy mayor de edad? " + esMayorEdad);
+
+    }
+
+}
+
