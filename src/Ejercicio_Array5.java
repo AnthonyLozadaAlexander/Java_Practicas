7dd9327bfa18a71f1bb7bdbf3fdddc0519511ab1
zDefcon20 <antholozadarequelme@gmail.com>
2025-02-07 01:33:31 -0500

    Modificar Ejercicio_Array5 para almacenar sueldos como double y calcular la suma y el promedio de los sueldos anuales.

--- src/Ejercicio_Array5.java
+++ src/Ejercicio_Array5.java
@@ -5,14 +5,24 @@ public class Ejercicio_Array5 {
       // Un empleado desea almacenar en un vector sus 12 sueldos del año.
       // A partir de esto, necesita un programa que determine y muestre por pantalla la suma total de sus 12 sueldos y el promedio de sueldo que tuvo en el año.
 
-      int sueldos [] = new int[12];
+      double sueldos [] = new double[12];
       Scanner input = new Scanner(System.in);
 
       // Carga de los sueldos
       for(int i = 0; i < sueldos.length; i++)
       {
-        System.out.print("Ingrese el sueldo del mes ["+i+"]: ");
-        sueldos[i] = input.nextInt();
+        System.out.print("Ingrese el sueldo del mes ["+(i+1)+"]: ");
+        sueldos[i] = input.nextDouble();
       }
+      double suma = 0;
+
+      for(int i = 0; i < sueldos.length; i++)
+      {
+        suma = suma + sueldos[i];
+      }
+      System.out.println("");
+      double promedio = suma / sueldos.length;
+      System.out.println("La suma total de los sueldos es: " + suma + " $");
+      System.out.println("El promedio de los sueldos es: " + promedio+ " dolares");
     }
 }
