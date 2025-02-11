93558f7b55963bc20b556b462faf14de6f293071
zDefcon20 <antholozadarequelme@gmail.com>
2025-02-11 17:23:52 -0500

    Agregar Ejercicio_9 para mostrar números hasta un límite ingresado por el usuario

--- src/Ejercicio_9.java
+++ src/Ejercicio_9.java
@@ -1,6 +1,18 @@
+import java.util.Scanner;
+
 public class Ejercicio_9 {
   public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    int numLimit, count;
+
+    System.out.println("Ingrese el limite de numeros a mostrar: ");
+    numLimit = input.nextInt();
+    count = 1;
     
-  }
-  
+    while(count <= numLimit){
+      System.out.println("num = " + count);
+      count = count + 1; // contador para que no sea un bucle infinito y llegue al numLimit
+    }
+    input.close(); // Cerramos el Scanner
+  } 
 }
