a048bea0bfec490bdbcbc76cf6fb3fcdf1b00003
zDefcon20 <antholozadarequelme@gmail.com>
2025-02-13 19:13:11 -0500

    Agregar Ejemplo_Matriz para ingresar y mostrar una matriz de 3x3

---
+++ src/Ejemplo_Matriz.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Ejemplo_Matriz {
+  public static void main(String[] args) {
+    int i, j;
+    Scanner input = new Scanner(System.in);
+
+    //? Matriz 3x3
+    int[][] matriz = new int[3][3]; // 3 filas y 3 columnas
+    for(i = 0; i < matriz.length; i++){
+      for(j = 0; j < matriz[i].length; j++){
+        System.out.print("Ingrese elemento a la Matriz[" + i + "][" + j + "]: ");
+        matriz[i][j] = input.nextInt();
+      }
+    }
+    System.out.println("");
+    System.out.println("Datos de la Matriz: ");
+    for(i = 0; i < matriz.length; i++){
+      for(j = 0; j < matriz[i].length; j++){
+        System.out.print(matriz[i][j]+ " ");
+      }
+      System.out.println();
+    }
+  }
+  
+}
