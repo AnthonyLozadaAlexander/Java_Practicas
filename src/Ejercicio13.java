006fa3f18cb1b114d7aa76d429f143feac4b3617
zDefcon20 <antholozadarequelme@gmail.com>
2025-02-22 00:02:05 -0500

    Agregar clase Ejercicio13 para simular el cálculo del salario total anual basado en ventas de carros.

---
+++ src/Ejercicio13.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Ejercicio13 {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    float salario, comision, totalVentas, meses, ventas, count, total;
+    salario = 1000;
+    comision = 150;
+    meses = 12;
+    count = 0;
+
+    System.out.println("----SIMULADOR DE SALARIO----");
+    for(int i = 1; i <= meses; i++){
+      System.out.println("Mes["+(i)+"]");
+      System.out.println("Ingrese Cuantos Carros Se Han Vendido: ");
+      ventas = input.nextFloat();
+      System.out.println("-------------------------------------");
+      totalVentas = salario + (ventas * comision);
+      System.out.println("El Salario Total Del Mes ["+i+"] Es: $"+totalVentas);
+      count = count + totalVentas;
+      System.out.println("-------------------------------------");
+    }
+    System.out.println("-------------------------------------");
+    System.out.println("El Salario Total Del Año Es De: " + count);
+  }
+}
