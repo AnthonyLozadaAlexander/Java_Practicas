25759aba3850e6f41ff5cb4426f2f1143f4ff1e9
zDefcon20 <antholozadarequelme@gmail.com>
2025-03-04 16:19:57 -0500

    Refactor Ejercicio3: mejorar la estructura del código y la validación de entrada en la tabla de multiplicar

--- src/EjerciciosFunciones/Ejercicio3.java
+++ src/EjerciciosFunciones/Ejercicio3.java
@@ -3,27 +3,28 @@ package EjerciciosFunciones;
 import java.util.Scanner;
 
 public class Ejercicio3 {
-  static int tablaMultiplicar(int num){
-    for(int i = 0; i <= 12; i++){
+  
+  public static int tablaMultiplicar(int num) {
+    for (int i = 0; i <= 12; i++) {
       System.out.println(num + " x " + i + " = " + (num * i));
     }
     return num;
   }
+
   public static void main(String[] args) {
     Scanner input = new Scanner(System.in);
     int num;
 
-    do{
-    System.out.println("Ingrese Un Número Para Mostrar Su Tabla De Multiplicar: ");
-    num = input.nextInt();
+    do {
+      System.out.println("Ingrese Un Número Para Mostrar Su Tabla De Multiplicar: ");
+      num = input.nextInt();
 
-    if(num <= 0){
-      System.out.println("Error: El número debe ser mayor a 0");
-    }
-    else{
-      tablaMultiplicar(num);
-    }
-    }while(num <= 0);
-    
-  } 
+      if (num <= 0) {
+        System.out.println("Error: El número debe ser mayor a 0");
+      } else {
+        tablaMultiplicar(num);
+      }
+    } while (num <= 0);
+
+  }
 }
