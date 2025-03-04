7a5c62046eaaa5dd96ba16a640daa6e7673fb4fc
zDefcon20 <antholozadarequelme@gmail.com>
2025-03-03 23:58:00 -0500

    Refactor el bucle de continuación en While_ejercicio y agregar validación de entrada para los números de hombres y mujeres

--- src/While_ejercicio.java
+++ src/While_ejercicio.java
@@ -6,12 +6,49 @@ public class While_ejercicio {
         boolean bandera = true;
         Scanner input = new Scanner(System.in);
 
-        while(bandera){
+        while(true) {
             System.out.println("¿Desea continuar? (s/n)");
             respuesta = input.next();
 
-            if(respuesta.equalsIgnoreCase("n")){
-                bandera = false;
+            if(respuesta.equalsIgnoreCase("n")) {
+            break; // alternativa a bandera = false;
+            } 
+            else if(respuesta.equalsIgnoreCase("s")) {
+                System.out.println("Continuando...");
+                System.out.println("---------------------------------------------------------");
+                System.out.println("------ Programa Que Calcula El Porcentaje De Hombres y Mujeres En Un Grupo -----");
+                System.out.println("---------------------------------------------------------");
+                int mujeres, hombres, total;
+                do {
+                    System.out.println("Ingrese el numero de mujeres");
+                    System.out.print("-> ");
+                    mujeres = input.nextInt();
+                    System.out.println("Ingrese el numero de hombres");
+                    System.out.print("-> ");
+                    hombres = input.nextInt();
+                    total = (mujeres + hombres);
+
+                    if (mujeres < 0 || mujeres == 0 || hombres == 0 || hombres < 0)
+                    {
+                        System.out.println("---------------------------------------------------------");
+                        System.out.println("Error: Los valores no pueden ser menores a 0 o iguales a 0");
+                        System.out.println("---------------------------------------------------------");
+                    } else {
+                        System.out.println("---------------------------------------------------------");
+                        System.out.println("Datos ingresados correctamente");
+                        System.out.println("---------------------------------------------------------");
+                    }
+                    mujeres = mujeres * 100/total;
+                    hombres = hombres * 100/total;
+
+                    System.out.println("\nEl porcentaje de mujeres es: " + mujeres + "%\n");
+                    System.out.println("\nEl porcentaje de hombres es: " + hombres + "%\n");
+                    System.out.println("---------------------------------------------------------");
+                } while(mujeres < 0 || mujeres == 0 || hombres == 0 || hombres < 0);
+                System.out.println("Continuando...");
+                System.out.println("---------------------------------------------------------");
+            } else {
+                System.out.println("Respuesta Invalida");
             }
         }
     }
