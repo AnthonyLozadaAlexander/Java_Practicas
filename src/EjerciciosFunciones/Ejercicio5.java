f4d4ab7c4555926072ba2dfc2bb6c1a4a178e3be
zDefcon20 <antholozadarequelme@gmail.com>
2025-03-05 18:51:45 -0500

    Agregar Ejercicio5: implementar función para determinar si un número es primo y mejorar la validación de entrada

--- src/EjerciciosFunciones/Ejercicio5.java
+++ src/EjerciciosFunciones/Ejercicio5.java
@@ -1,7 +1,61 @@
 package EjerciciosFunciones;
 
+import java.util.Scanner;
+
 public class Ejercicio5 {
+
+  public static boolean esPrimo(int n){
+    if(n <= 1){
+      return false;
+    }
+    else if(n == 2){
+      return true;
+    }
+    else
+    {
+      for(int i = 2; i < n; i++)
+      {
+        if(n % i == 0)
+        {
+          return false;
+        }
+      }
+
+      return true;
+
+    }
+  }
+
   public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    int num;
+    System.out.println("Ejercicio 5: Determinar si un número es primo o no");
+    System.out.println("--------------------------------------------------");
+    
+    do{
+    System.out.println("Ingrese hasta que numero desde el 1 hasta n saber si es primo o no:");
+    System.out.println("-> ");
+    if(!input.hasNextInt()){
+      System.out.println("Error: Debe Ingresar Un Numero Entero");
+      return;
+    }
+    num = input.nextInt();
+
+    System.out.println("--------------------------------------------------");
+    if(num <= 1){
+      System.out.println("Error: Debe Ingresar Un Numero Mayor A 0");
+      return;
+    }
+    else{
+
+      for(int i = 1; i <= num; i++){
+        System.out.println("i = " + i + " es primo? -> " + esPrimo(i));
+      }
+      
+    }
+  }while(num <= 1  || !input.hasNextInt());
+    input.close();
+    System.out.println("--------------------------------------------------");
     
   }
 }
