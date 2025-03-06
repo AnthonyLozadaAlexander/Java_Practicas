722d6096a9eaa543daf5c3790c13f4814d977292
zDefcon20 <antholozadarequelme@gmail.com>
2025-03-05 19:12:44 -0500

    Refactor Ejercicio5: mejorar la validación de entrada y la presentación de resultados al determinar si un número es primo

--- src/EjerciciosFunciones/Ejercicio5.java
+++ src/EjerciciosFunciones/Ejercicio5.java
@@ -4,19 +4,14 @@ import java.util.Scanner;
 
 public class Ejercicio5 {
 
-  public static boolean esPrimo(int n){
-    if(n <= 1){
+  public static boolean esPrimo(int n) {
+    if (n <= 1) {
       return false;
-    }
-    else if(n == 2){
+    } else if (n == 2) {
       return true;
-    }
-    else
-    {
-      for(int i = 2; i < n; i++)
-      {
-        if(n % i == 0)
-        {
+    } else {
+      for (int i = 2; i < n; i++) {
+        if (n % i == 0) {
           return false;
         }
       }
@@ -28,34 +23,37 @@ public class Ejercicio5 {
 
   public static void main(String[] args) {
     Scanner input = new Scanner(System.in);
-    int num;
+    int num = 0;
     System.out.println("Ejercicio 5: Determinar si un número es primo o no");
     System.out.println("--------------------------------------------------");
-    
-    do{
-    System.out.println("Ingrese hasta que numero desde el 1 hasta n saber si es primo o no:");
-    System.out.println("-> ");
-    if(!input.hasNextInt()){
-      System.out.println("Error: Debe Ingresar Un Numero Entero");
-      return;
-    }
-    num = input.nextInt();
 
-    System.out.println("--------------------------------------------------");
-    if(num <= 1){
-      System.out.println("Error: Debe Ingresar Un Numero Mayor A 0");
-      return;
-    }
-    else{
+    do {
+      System.out.println("\nIngrese hasta que numero desde el 1 hasta n saber si es primo o no:");
+      System.out.print("-> ");
 
-      for(int i = 1; i <= num; i++){
-        System.out.println("i = " + i + " es primo? -> " + esPrimo(i));
+      if (!input.hasNextInt()) {
+        System.out.println("\nError: Debe Ingresar Un Numero Entero\n");
+        input.next(); // discard invalid input
+        continue;
       }
-      
-    }
-  }while(num <= 1  || !input.hasNextInt());
-    input.close();
-    System.out.println("--------------------------------------------------");
-    
+      num = input.nextInt();
+
+      if (num <= 1) {
+        System.out.println("\nError: Debe Ingresar Un Numero Mayor A 0\n");
+        continue;
+      } 
+      else {
+        System.out.println("--------------------------------------------------");
+        for (int i = 1; i <= num; i++) {
+          System.out.println("i = " + i + " es primo? -> " + esPrimo(i));
+        }
+        System.out.println("--------------------------------------------------");
+        break;
+      }
+
+    } while (num <= 1 || !input.hasNextInt());
+
+    System.out.println("\nFin del programa. Gracias por participar.");
+
   }
 }
