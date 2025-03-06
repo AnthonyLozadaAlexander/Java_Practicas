78de620142fe76034e000e090be6b4996bc2ea5a
zDefcon20 <antholozadarequelme@gmail.com>
2025-03-05 19:16:45 -0500

    Refactor Ejercicio5: simplificar la estructura de control en la función esPrimo

--- src/EjerciciosFunciones/Ejercicio5.java
+++ src/EjerciciosFunciones/Ejercicio5.java
@@ -7,9 +7,11 @@ public class Ejercicio5 {
   public static boolean esPrimo(int n) {
     if (n <= 1) {
       return false;
-    } else if (n == 2) {
+    } 
+    else if (n == 2) {
       return true;
-    } else {
+    }
+    else {
       for (int i = 2; i < n; i++) {
         if (n % i == 0) {
           return false;
