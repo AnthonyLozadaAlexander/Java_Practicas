d90a95dd1bc9b132437e340a73c37b6dd20cbd65
zDefcon20 <antholozadarequelme@gmail.com>
2025-03-21 17:23:05 -0500

    Agregar Ejercicio24: mejorar la validación de entrada para litros y precios en la calculadora de facturación

--- src/Ejercicios/Ejercicio24.java
+++ src/Ejercicios/Ejercicio24.java
@@ -8,51 +8,60 @@ public class Ejercicio24 {
         String codigo = "";
         float cantLitros = 0.0f, precioLitros = 0.0f, montoTotal=0.0f, facturacionTotal=0.0f, countLitros = 0.0f, countFacturas=0.0f;
 
+        productosLoop:
         for(int c = 1; c <= 5; c++) {
           input = new Scanner(System.in);
             System.out.println("\nIteracion(" + c + ")");
             System.out.println("Ingrese El Codigo Del Producto");
-            System.out.println("-> ");
+            System.out.print("-> ");
             codigo = input.nextLine();
 
             if(codigo.equalsIgnoreCase("")){
                 System.out.println("\nError: No Puede Estar Vacio\n");
                 c = c - 1;
             }
-            else{
+            else {
                 System.out.println("\nCodigo Ingresado: " + codigo + "\n");
-            }
-            for (int i = 1; i <= 5; i++) {
-                input = new Scanner(System.in);
-                System.out.println("Ingrese La Cantidad De Litros");
-                System.out.print("-> ");
-                cantLitros = input.nextFloat();
 
-                if (cantLitros <= 0) {
-                    System.out.println("Error: Ingrese un valor mayor que 0");
-                    i = i - 1;
-                } else {
-                    System.out.println("\nCantidad de Litros Ingresada: " + cantLitros + "\n");
+                litrosLoop:
+                for (int i = 1; i <= 5; i++) {
+                    input = new Scanner(System.in);
+                    System.out.println("Ingrese La Cantidad De Litros");
+                    System.out.print("-> ");
+                    cantLitros = input.nextFloat();
 
-                    for (int j = 1; j <= 5; j++) {
-                        input = new Scanner(System.in);
-                        System.out.println("Ingrese El Precio Por Litro");
-                        System.out.print("-> ");
-                        precioLitros = input.nextFloat();
+                    if (cantLitros <= 0) {
+                        System.out.println("Error: Ingrese un valor mayor que 0");
+                        i = i - 1;
+                    }
+                    else {
+                        System.out.println("\nCantidad de Litros Ingresados: " + cantLitros + "\n");
 
-                        if (precioLitros <= 0) {
-                            System.out.println("Error: Ingrese un valor mayor que 0");
-                            j = j - 1;
-                        } else {
-                            facturacionTotal = (cantLitros * precioLitros);
-                            montoTotal = montoTotal + facturacionTotal;
-                            countLitros = countLitros + cantLitros;
+                        for (int j = 1; j <= 5; j++) {
+                            input = new Scanner(System.in);
+                            System.out.println("Ingrese El Precio Por Litro");
+                            System.out.print("-> ");
+                            precioLitros = input.nextFloat();
 
-                            if (facturacionTotal > 600) {
-                                countFacturas = countFacturas + 1;
+                            if (precioLitros <= 0) {
+                                System.out.println("\nError: Ingrese un valor mayor que 0\n");
+                                j = j - 1;
+                                /*j = 5; // Salir del bucle de precio por litro
+                                i = 5; // Salir del bucle de cantidad de litros
+                                c = c - 1; // Volver a ingresar el código del producto*/
+                            } else {
+
+                                System.out.println("\nPrecio Por Litro Ingresado: " + precioLitros + "\n");
+                                facturacionTotal = (cantLitros * precioLitros);
+                                montoTotal = montoTotal + facturacionTotal;
+                                countLitros = countLitros + cantLitros;
+
+                                if (facturacionTotal > 600) {
+                                    countFacturas = countFacturas + 1;
+                                }
+
+                                continue productosLoop;
                             }
-
-                            break;
                         }
                     }
                 }
