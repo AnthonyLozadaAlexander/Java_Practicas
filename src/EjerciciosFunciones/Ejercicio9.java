fc755afba020015b0be17f26063c8b6e5073bca4
zDefcon20 <antholozadarequelme@gmail.com>
2025-03-23 02:02:03 -0500

    Agregar Ejercicio9: implementar función para validar si una palabra es un palíndromo

--- src/EjerciciosFunciones/Ejercicio9.java
+++ src/EjerciciosFunciones/Ejercicio9.java
@@ -3,6 +3,31 @@ package EjerciciosFunciones;
 import java.util.Scanner;
 
 public class Ejercicio9 {
+
+    public static boolean esPalindromo(String palabra){
+        palabra = palabra.toLowerCase(); // la palabra ingresada se volvera minuscula para su proceso de evaluacion
+        palabra = palabra.replace(" ", ""); // se remplazan los espacios ingresados en la palabra para eliminarlos y hacer la palabra toda junta
+        palabra = palabra.replace("á", "a");
+        palabra = palabra.replace("é", "e");
+        palabra = palabra.replace("í", "i");
+        palabra = palabra.replace("ó", "o");
+        palabra = palabra.replace("ú", "u");
+
+        int a = 0; // primer indice de la palabra
+        int b = palabra.length() - 1; // ultimo indice de la palabra
+
+        for(int i = 0; i < palabra.length(); i++){
+            if(palabra.charAt(i) == palabra.charAt(b)){
+                a++;
+                b--;
+            }
+            else{
+              return false;
+            }
+
+        }
+    }
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         String palabra;
