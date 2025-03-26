0e0e5fdef652f7ba5928a56600a595e5f3ac2761
zDefcon20 <antholozadarequelme@gmail.com>
2025-03-26 16:33:06 -0500

    Agregar MediaDeArreglos: mejorar la entrada de datos enteros con manejo de errores y conteo de positivos, negativos y ceros

--- src/EjerciciosArrays/MediaDeArreglos.java
+++ src/EjerciciosArrays/MediaDeArreglos.java
@@ -11,6 +11,8 @@ public class MediaDeArreglos {
         System.out.println("     BIENVENIDO");
         System.out.println("--------------------");
         System.out.println("Ingrese Los Datos Enteros Al Array");
+
+        resetLoop:
         for(int i = 0; i < nums.length; i++) {
             System.out.println("nums["+i+"]");
             System.out.print("-> ");
@@ -18,6 +20,26 @@ public class MediaDeArreglos {
             if(!input.hasNextInt()){
                 System.out.println("\nError: Debe Ingresar Un Numero Entero\n");
                 input.next();
+                if(i > 0 || i < nums.length){
+                    i = i - 1;
+                }
+                else{
+                    continue resetLoop;
+                }
+            }
+            nums[i] = input.nextInt();
+
+            if(nums[i] > 0){
+                System.out.println("Dato Positivo Ingresado -> " + nums[i]);
+                sumP = sumP + nums[i];
+                countP++; // el contador se ira sumando cada que se ingrese numeros positivos
+            }
+            else if(nums[i] < 0){
+                System.out.println("Dato Negativo Ingresado -> " + nums[i]);
+                sumN = sumN + nums[i];
+                countN++; // contador para los negativos
+            }else{
+                count0++; // contador para los ceros
             }
         }
     }
