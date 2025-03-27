e83442e99a459a07f89f05a35325779f1765383d
zDefcon20 <antholozadarequelme@gmail.com>
2025-03-26 19:00:52 -0500

    Agregar TablasArrays: renombrar variables y mejorar la legibilidad de la entrada y salida de datos en los arrays

--- src/EjerciciosArrays/TablasArrays.java
+++ src/EjerciciosArrays/TablasArrays.java
@@ -5,40 +5,52 @@ import java.util.Scanner;
 public class TablasArrays {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int[] Tabla1 = new int[10];
-        int[] Tabla2 = new int[10];
-        int[] array3 = new int[20];
+        int[] A = new int[10];
+        int[] B = new int[10];
+        int[] C = new int[20];
 
         System.out.println("-----------------------------");
         System.out.println("         BIENVENIDO");
         System.out.println("-----------------------------");
 
-        System.out.println("Ingrese Datos Enteros Al Array1");
-        for(int i = 0; i < Tabla1.length; i++){
+        System.out.println("Ingrese Datos Enteros Al Array A");
+        for(int i = 0; i < A.length; i++){
             System.out.println("Array1["+i+"]");
             System.out.print("-> ");
-            Tabla1[i] = input.nextInt();
+            A[i] = input.nextInt();
         }
 
-        System.out.println("\nDatos Enteros Del Array1");
-        for(int i = 0; i < Tabla1.length; i++){
-            System.out.println("Array["+i+"] = "+ Tabla1[i]);
+        System.out.println("\nDatos Enteros Del Array A");
+        for(int i = 0; i < A.length; i++){
+            System.out.println("Array["+i+"] = "+ A[i]);
         }
 
-        System.out.println("\nIngrese Datos Enteros Al Array2");
-        for(int j = 0; j < Tabla2.length; j++){
+        System.out.println("\nIngrese Datos Enteros Al Array B");
+        for(int j = 0; j < B.length; j++){
             System.out.println("Array2["+j+"]");
             System.out.print("-> ");
-            Tabla2[j] = input.nextInt();
+            B[j] = input.nextInt();
         }
 
-        System.out.println("\nDatos Enteros Del Array2");
-        for(int j = 0; j < Tabla2.length; j++){
-            System.out.println("Array["+j+"] = "+ Tabla1[j]);
+        System.out.println("\nDatos Enteros Del Array B");
+        for(int j = 0; j < B.length; j++){
+            System.out.println("Array["+j+"] = "+ B[j]);
         }
 
-        for(int k = 0; k < array3.length; k++){
+        for(int k = 0; k < C.length; k++){
+            if(k % 2 == 0){
+                C[k] = B[k];
+            }
+            else{
+                C[k] = A[k];
+            }
+        }
 
+        System.out.println("\nDatos Del Arreglo C");
+        int countC = 0;
+        for(int datos: C){
+            System.out.println("C["+countC+"] = "+ datos);
+            countC++;
         }
     }
 }
