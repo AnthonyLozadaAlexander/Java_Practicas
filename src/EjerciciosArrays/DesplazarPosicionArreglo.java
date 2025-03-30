3c600813fa0f600df40527fdc4e3e0098d4e5901
zDefcon20 <antholozadarequelme@gmail.com>
2025-03-29 23:52:46 -0500

    refactor(DesplazarPosicionArreglo): renombrar variable de 'Elements' a 'Vector' para mayor claridad

--- src/EjerciciosArrays/DesplazarPosicionArreglo.java
+++ src/EjerciciosArrays/DesplazarPosicionArreglo.java
@@ -5,33 +5,33 @@ import java.util.Scanner;
 public class DesplazarPosicionArreglo {
     public static void main(String[] args) {
        Scanner input = new Scanner(System.in);
-       int[] Elements = new int[10];
+       int[] Vector = new int[10];
        int last = 0;
 
        System.out.println("Ingrese los 10 elementos al arreglo");
-       for(int i = 0; i < Elements.length; i++)
+       for(int i = 0; i < Vector.length; i++)
        {
         System.out.println("Element["+i+"]");
        System.out.print("-> ");
-       Elements[i] = input.nextInt();
+       Vector[i] = input.nextInt();
        }
 
        System.out.println("Los elementos del arreglo son: ");
-       for(int k = 0; k < Elements.length; k++){
-        System.out.println("Element["+k+"] = " + Elements[k]);
+       for(int k = 0; k < Vector.length; k++){
+        System.out.println("Element["+k+"] = " + Vector[k]);
        }
        
-       last = Elements[9]; // guardamos el ultimo elemento del array en una variable
+       last = Vector[9]; // guardamos el ultimo elemento del array en una variable
        
        for(int j = 8; j >= 0; j--){ // bajando elementos en el array
-        Elements[j+1] = Elements[j]; // Elements[9] = Elements[8], Elements[8] = Elements[7], etc.
+        Vector[j+1] = Vector[j]; // Elements[9] = Elements[8], Elements[8] = Elements[7], etc.
        }
 
-       Elements[0] = last; // guardamos el ultimo elemento en el primer elemento del array
+       Vector[0] = last; // guardamos el ultimo elemento en el primer elemento del array
 
-       System.out.println("Los elementos del arreglo son: ");
+       System.out.println("Los elementos del arreglo modificado son: ");
        int count = 0;
-       for(int datos: Elements){
+       for(int datos: Vector){
         System.out.println("Element["+count+"] = " + datos);
         count++;
        }
