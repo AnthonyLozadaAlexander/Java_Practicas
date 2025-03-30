c17892070e188424f292f91593760731dff67590
zDefcon20 <antholozadarequelme@gmail.com>
2025-03-29 23:52:37 -0500

    refactor(ArregloNDesplazamientos): renombrar variable de 'Elements' a 'Vector' y mejorar mensajes de entrada

--- src/EjerciciosArrays/ArregloNDesplazamientos.java
+++ src/EjerciciosArrays/ArregloNDesplazamientos.java
@@ -5,25 +5,30 @@ import java.util.Scanner;
 public class ArregloNDesplazamientos {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int[] Elements = new int[10];
+        int[] Vector = new int[10];
         int n = 0;
         System.out.println("-----------------------------");
         System.out.println("          BIENVENIDO");
         System.out.println("-----------------------------");
         System.out.println("Ingrese los 10 elementos al arreglo");
-        for(int i = 0; i < Elements.length; i++){
+        for(int i = 0; i < Vector.length; i++){
             System.out.println("Element["+i+"]");
             System.out.print("-> ");
             if(!input.hasNextInt()){
-                System.out.println("ERROR: Debe Ingresar Un Numero Entero");
+                System.out.println("\nERROR: Debe Ingresar Un Numero Entero\n");
                 input.next();
                 i--;
                 continue;
             }
             else{
-            Elements[i] = input.nextInt();
-            System.out.println("Elemento["+i+"] Ingresado: " + Elements[i]);
+            Vector[i] = input.nextInt();
+            System.out.println("\nIndice["+i+"] Ingresado: " + Vector[i] + "\n");
             }
+        }
+
+        System.out.println("Datos Del Arreglo");
+        for(int i = 0; i < Vector.length; i++){
+
         }
     }
 }
