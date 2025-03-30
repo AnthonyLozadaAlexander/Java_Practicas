dca53cf1c70981736c0365a0e4ec1eaeb89ff74c
zDefcon20 <antholozadarequelme@gmail.com>
2025-03-29 23:44:48 -0500

    feat(DesplazarPosicionArreglo): agregar clase para desplazar elementos en un arreglo

---
+++ src/EjerciciosArrays/DesplazarPosicionArreglo.java
@@ -0,0 +1,34 @@
+package EjerciciosArrays;
+
+import java.util.Scanner;
+
+public class DesplazarPosicionArreglo {
+    public static void main(String[] args) {
+       Scanner input = new Scanner(System.in);
+       int[] Elements = new int[10];
+       int last = 0;
+
+       System.out.println("Ingrese los 10 elementos al arreglo");
+       for(int i = 0; i < Elements.length; i++)
+       {
+        System.out.println("Element["+i+"]");
+       System.out.print("-> ");
+       Elements[i] = input.nextInt();
+       }
+       last = Elements[9]; // guardamos el ultimo elemento del array en una variable
+       
+       for(int j = 8; j >= 0; j--){ // bajando elementos en el array
+        Elements[j+1] = Elements[j];
+       }
+
+       Elements[0] = last; // guardamos el ultimo elemento en el primer elemento del array
+
+       System.out.println("Los elementos del arreglo son: ");
+       int count = 0;
+       for(int datos: Elements){
+        System.out.println("Element["+count+"] = " + datos);
+        count++;
+       }
+
+    }
+}
