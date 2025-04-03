c9fc1c62c53c35b0e780a0179912b004bb1a814c
zDefcon20 <antholozadarequelme@gmail.com>
2025-04-03 18:32:52 -0500

    fix(InsertarNumEnArreglo): corregir lógica de verificación de orden creciente en el arreglo

--- src/EjerciciosArrays/InsertarNumEnArreglo.java
+++ src/EjerciciosArrays/InsertarNumEnArreglo.java
@@ -20,14 +20,21 @@ public class InsertarNumEnArreglo {
                 array[i] = input.nextInt();
             }
             // Comprobar si el arreglo se encuentra ordenado en forma creciente
-            for(int c = 0; c < array.length; c++){
-                if(array[c] < array[c+1]){ // (C[0] = 1 < C[1] = 2) == true
+            // Creciente = 1-2-3-4-5-6-7
+
+            for(int j = 0; j < array.length; j++){
+                if(array[j] < array[j+1]){ // (C[0] = 1 < C[1] = 2) == true
                     esCreciente = true;
                 }
-                else{
+
+                if(array[j] > array[j+1]){ // decreciente = 5-4-3-2-1
                     esCreciente = false; // (C[0] = 5 < C[1] = 4) == false
                 }
             }
-        }while(!esCreciente);
+
+            if(esCreciente == false){
+                System.out.println("\nEl Arreglo No Se Encuentra Ordenado Crecientemente\n");
+            }
+        }while(!esCreciente); // Mientras(esCreciente == false);
     }
 }
