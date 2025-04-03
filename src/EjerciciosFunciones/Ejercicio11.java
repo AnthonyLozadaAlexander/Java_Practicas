ffc798e45877211950b00fd86b5669c7e03ebdcf
zDefcon20 <antholozadarequelme@gmail.com>
2025-04-03 14:51:53 -0500

    feat(Ejercicio11): agregar clase y método principal para calcular el área de un rectángulo

---
+++ src/EjerciciosFunciones/Ejercicio11.java
@@ -0,0 +1,11 @@
+package EjerciciosFunciones;
+
+public class Ejercicio11 {
+    public static void main(String[] args) {
+        float base = 0.0f, altura = 0.0f;
+        System.out.println("----------------------------------------");
+        System.out.println("              BIENVENIDO");
+        System.out.println("-----------------------------------------");
+        System.out.println("Ingrese la base del rectangulo");
+    }
+}
