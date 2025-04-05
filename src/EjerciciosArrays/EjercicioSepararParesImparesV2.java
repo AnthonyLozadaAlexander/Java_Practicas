ca60aedb29fca9fbd613c758dc2225c363e08c2a
zDefcon20 <antholozadarequelme@gmail.com>
2025-04-04 19:39:27 -0500

    feat(EjercicioSepararParesImparesV2): mejorar la interacción del usuario al ingresar elementos en el arreglo

--- src/EjerciciosArrays/EjercicioSepararParesImparesV2.java
+++ src/EjerciciosArrays/EjercicioSepararParesImparesV2.java
@@ -6,5 +6,22 @@ public class EjercicioSepararParesImparesV2 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int[] array = new int[10];
+
+        System.out.println("-------------------------");
+        System.out.println("        BIENVENIDO");
+        System.out.println("-------------------------");
+        System.out.println("Ingrese los elementos al arreglo");
+        for(int i = 0; i < array.length; i++){
+            System.out.println("array["+i+"] = " + array[i]);
+            System.out.print("-> ");
+            if(!input.hasNextInt()){
+                System.out.println("\nError: Dato Invalido, Ingrese Un Numero Entero\n");
+                input.nextInt();
+                i = i-1;
+            }
+            else {
+                array[i] = input.nextInt();
+            }
+        }
     }
 }
