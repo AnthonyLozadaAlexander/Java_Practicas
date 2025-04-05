78b8da8704f2bc939961cae27fb33be441cf9551
zDefcon20 <antholozadarequelme@gmail.com>
2025-04-04 21:51:46 -0500

    feat(BuscarElementoArreglo): inicializar arreglo y agregar estructura para entrada de datos

--- src/EjerciciosArrays/BuscarElementoArreglo.java
+++ src/EjerciciosArrays/BuscarElementoArreglo.java
@@ -5,5 +5,11 @@ import java.util.Scanner;
 public class BuscarElementoArreglo {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        int arreglo[], num;
+        arreglo = new int[10];
+        do{
+        // Pedimos el arreglo
+
+        }while();
     }
 }
