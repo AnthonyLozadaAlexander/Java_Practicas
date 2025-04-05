ade3ec23afee71cad981388f9c3f2ebb0b83c275
zDefcon20 <antholozadarequelme@gmail.com>
2025-04-04 21:46:50 -0500

    feat(BuscarElementoArreglo): agregar clase para buscar elementos en un arreglo

---
+++ src/EjerciciosArrays/BuscarElementoArreglo.java
@@ -0,0 +1,9 @@
+package EjerciciosArrays;
+
+import java.util.Scanner;
+
+public class BuscarElementoArreglo {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+    }
+}
