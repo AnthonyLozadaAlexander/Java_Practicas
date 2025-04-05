59c5c47debe693d0e6b0cb8dc0ff247e1a35ee49
zDefcon20 <antholozadarequelme@gmail.com>
2025-04-05 01:20:15 -0500

    feat(Ejercicio14): mejorar la entrada del usuario con validación para el número

--- src/EjerciciosFunciones/Ejercicio14.java
+++ src/EjerciciosFunciones/Ejercicio14.java
@@ -3,29 +3,34 @@ package EjerciciosFunciones;
 import java.util.Scanner;
 
 public class Ejercicio14 {
-    public static float absoluto(float num){
+    public static float absoluto(float num) {
         float R = 0.0f;
 
-        if(num < 0)
-        {
+        if (num < 0) {
             R = -num;
-        }
-        else
-        {
+        } else {
             R = num;
         }
 
         return R;
     }
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         float num = 0.0f;
-do {
-    System.out.println("Digite un numero");
-    System.out.print("-> ");
-
-
-    num = input.nextFloat();
-}while(true);
+        do {
+            System.out.println("Digite un numero");
+            System.out.print("-> ");
+            if(!input.hasNextFloat()) {
+                System.out.println("\nError: Debe Ingresar Un Numero\n");
+                continue;
+            }
+            else{
+                num = input.nextFloat();
+                System.out.println("\nNumero Ingresado: " + num);
+                System.out.println("Valor Absoluto De "+num+" = " + absoluto(num));
+                break;
+            }
+        } while (true);
     }
 }
