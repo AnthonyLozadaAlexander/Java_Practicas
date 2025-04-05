fd34650e3589b25ca0f6af0349dd8a3a0a2dd682
zDefcon20 <antholozadarequelme@gmail.com>
2025-04-04 19:47:28 -0500

    feat(EjercicioSepararParesImparesV2): mejorar la visualización de la entrada de elementos y corregir la lógica para manejar ceros

--- src/EjerciciosArrays/EjercicioSepararParesImparesV2.java
+++ src/EjerciciosArrays/EjercicioSepararParesImparesV2.java
@@ -12,7 +12,7 @@ public class EjercicioSepararParesImparesV2 {
         System.out.println("-------------------------");
         System.out.println("Ingrese los elementos al arreglo");
         for(int i = 0; i < array.length; i++){
-            System.out.println("array["+i+"] = " + array[i]);
+            System.out.println("array["+i+"]");
             System.out.print("-> ");
             if(!input.hasNextInt()){
                 System.out.println("\nError: Dato Invalido, Ingrese Un Numero Entero\n");
@@ -23,8 +23,7 @@ public class EjercicioSepararParesImparesV2 {
                 array[i] = input.nextInt();
                 if(array[i] == 0){
                     System.out.println("\nError: Debe Ingresar Numeros Distintos De 0\n");
-                    i=i+1;
-                    i = i-1;
+                    i--;
                 }else {
                     System.out.println("Elemento Ingresado: " + "array[" + i + "] = " + array[i]);
                 }
