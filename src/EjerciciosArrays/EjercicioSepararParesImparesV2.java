3949e80859bf3b51f11928ed4c3df1069ae5b821
zDefcon20 <antholozadarequelme@gmail.com>
2025-04-04 19:35:59 -0500

    feat(EjercicioSepararParesImparesV2): agregar estructura inicial para separar números pares e impares

---
+++ src/EjerciciosArrays/EjercicioSepararParesImparesV2.java
@@ -0,0 +1,10 @@
+package EjerciciosArrays;
+
+import java.util.Scanner;
+
+public class EjercicioSepararParesImparesV2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int[] array = new int[10];
+    }
+}
