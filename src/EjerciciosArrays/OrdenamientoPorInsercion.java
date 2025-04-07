04006b219d174bc1ae952c7ac1ebd73057e288e3
zDefcon20 <antholozadarequelme@gmail.com>
2025-04-06 23:42:48 -0500

    refactor(OrdenamientoPorInsercion): mejorar la estructura del código y la validación de entrada para el tamaño del arreglo

--- src/EjerciciosArrays/OrdenamientoPorInsercion.java
+++ src/EjerciciosArrays/OrdenamientoPorInsercion.java
@@ -5,20 +5,29 @@ import java.util.Scanner;
 public class OrdenamientoPorInsercion {
   public static void main(String[] args) {
     Scanner input = new Scanner(System.in);
-    int arreglo[] , nElementos;
+    int arreglo[], nElementos = 0;
 
-    do{
-    System.out.println("Ingrese el tamaño del arreglo");
-    System.out.print("-> ");
-    nElementos = input.nextInt();
-    if(nElementos <= 0){
-      System.out.println("\nEl tamaño del arreglo debe ser mayor a 0");
-      System.out.println("Intente nuevamente\n");
-    }else{
-      System.out.println("\nTamaño Ingresado: " + nElementos);
-    }
-    }while(nElementos <= 0);
+    System.out.println("---------------------------------------------");
+    System.out.println("            ORDENAMIENTO POR INSERCION          ");
+    System.out.println("---------------------------------------------\n");
+    do {
+      System.out.println("Ingrese el tamaño del arreglo");
+      System.out.print("-> ");
+      if (!input.hasNextInt()) {
+        System.out.println("\nEl tamaño del arreglo debe ser un numero Entero\n");
+        input.next();
+        continue;
+      }
+      nElementos = input.nextInt();
+      if (nElementos <= 0) {
+        System.out.println("\nEl tamaño del arreglo debe ser mayor a 0");
+        System.out.println("Intente nuevamente\n");
+      } else {
+        System.out.println("\nTamaño Ingresado: " + nElementos + "\n");
+      }
+    } while (nElementos <= 0);
+
     arreglo = new int[nElementos];
-    
+
   }
 }
