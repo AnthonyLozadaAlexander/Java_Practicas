d69e9476e3cdaaa77da0136332f4c9e822d84596
zDefcon20 <antholozadarequelme@gmail.com>
2025-04-06 23:39:59 -0500

    feat(OrdenamientoPorInsercion): implementar entrada de usuario para el tamaño del arreglo con validación

--- src/EjerciciosArrays/OrdenamientoPorInsercion.java
+++ src/EjerciciosArrays/OrdenamientoPorInsercion.java
@@ -1,8 +1,24 @@
 package EjerciciosArrays;
 
+import java.util.Scanner;
+
 public class OrdenamientoPorInsercion {
   public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    int arreglo[] , nElementos;
+
+    do{
+    System.out.println("Ingrese el tamaño del arreglo");
+    System.out.print("-> ");
+    nElementos = input.nextInt();
+    if(nElementos <= 0){
+      System.out.println("\nEl tamaño del arreglo debe ser mayor a 0");
+      System.out.println("Intente nuevamente\n");
+    }else{
+      System.out.println("\nTamaño Ingresado: " + nElementos);
+    }
+    }while(nElementos <= 0);
+    arreglo = new int[nElementos];
     
   }
-
 }
