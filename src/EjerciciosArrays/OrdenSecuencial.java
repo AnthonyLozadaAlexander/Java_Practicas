494836c6636737e96d2083cd1354f03236dfa338
zDefcon20 <antholozadarequelme@gmail.com>
2025-04-12 17:43:21 -0500

    feat(OrdenSecuencial): implementar búsqueda secuencial en un arreglo

---
+++ src/EjerciciosArrays/OrdenSecuencial.java
@@ -0,0 +1,26 @@
+package EjerciciosArrays;
+
+import java.util.Scanner;
+public class OrdenSecuencial {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int [] arreglo = {4,1,5,2,3};
+        int dato;
+        boolean flag = false;
+
+        System.out.println("|----------------------------|");
+        System.out.println("|          BIENVENIDO        |");
+        System.out.println("|----------------------------|");
+        System.out.println("Ingrese el numero a buscar");
+        System.out.print("-> ");
+        dato = input.nextInt();
+
+        // Busqueda Secuencial
+        int i = 0;
+        // Mientras se recorra todo el array y no se haya encontrado el numero
+        while(i < 5 && flag == false){
+
+        };
+
+    }
+}
