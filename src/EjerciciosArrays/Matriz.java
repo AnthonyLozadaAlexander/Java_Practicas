e4517fd08845ab61465154ecf3c3554d4e917da0
zDefcon20 <antholozadarequelme@gmail.com>
2025-04-17 00:43:13 -0500

    refactor(Matrices): renombrar clase Matrices a Matriz y mejorar la legibilidad del código

--- src/EjerciciosArrays/Matriz.java
+++ src/EjerciciosArrays/Matriz.java
@@ -1,6 +1,6 @@
 package EjerciciosArrays;
 
-public class Matrices {
+public class Matriz {
     public static void main(String[] args) {
         int matriz[][] = {{1,2,3},
                           {4,5,6},
@@ -10,10 +10,9 @@ public class Matrices {
         {
             for(int j = 0; j < 3; j++) // columnas
             {
-
+                System.out.print(matriz[i][j]);
             }
+            System.out.println("");
         }
-
-
     }
 }
