911a6aa9254947b4c3e76a509734f3f24cb3ca2a
zDefcon20 <antholozadarequelme@gmail.com>
2025-04-16 23:57:29 -0500

    refactor(MatrizMagica_Impar): mejorar la legibilidad del código y la indentación

--- src/EjerciciosArrays/MatrizMagica_Impar.java
+++ src/EjerciciosArrays/MatrizMagica_Impar.java
@@ -2,62 +2,58 @@ package EjerciciosArrays;
 
 public class MatrizMagica_Impar {
     public static void main(String[] args) {
-            int n = 3;
+        int n = 3;
 
-            int i_Anterior = 0;
-            int j_Anterior = 0;
+        int i_Anterior = 0;
+        int j_Anterior = 0;
 
-            int puntoInicial = n / 2;
+        int puntoInicial = n / 2;
 
-            int[][] matrizMagica = new int[n][n];
-            int temp = 1;
+        int[][] matrizMagica = new int[n][n];
+        int temp = 1;
 
-            for(int i = 0; i < n; i++){
-                for(int j = 0; j < n; j++){
-                    matrizMagica[i][j] = 0;
-                }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                matrizMagica[i][j] = 0;
+            }
+        }
+
+        int i = 0;
+        int j = puntoInicial;
+
+        while (temp != (n * n) + 1) {
+            if (matrizMagica[i][j] == 0) { // verifica si esta vacio
+                matrizMagica[i][j] = temp;
+            } else {
+                i = i_Anterior + 1; // baja una posicion
+                j = j_Anterior;
+                matrizMagica[i][j] = temp;
             }
 
-            int i = 0;
-            int j = puntoInicial;
+            i_Anterior = i;
+            j_Anterior = j;
 
-            while(temp != (n*n)+1){
-                if(matrizMagica[i][j] == 0){ // verifica si esta vacio
-                    matrizMagica[i][j] = temp;
-                }
-                else{
-                    i = i_Anterior + 1; // baja una posicion
-                    j = j_Anterior;
-                    matrizMagica[i][j] = temp;
-                }
+            temp++;
+            j++;
+            i--;
 
-                i_Anterior = i;
-                j_Anterior = j;
-
-                temp++;
-                j++;
-                i--;
-
-                if(i < 0 && j == n){ // esquina superior
-                    i = n - 1;
-                    j = 0;
-                }
-                else if(i < 0)
-                {
-                    i = i + n;
-                }
-                else if(j == n){
-                    j = 0;
-                }
-
+            if (i < 0 && j == n) { // esquina superior
+                i = n - 1;
+                j = 0;
+            } else if (i < 0) {
+                i = i + n;
+            } else if (j == n) {
+                j = 0;
             }
 
-            for(int x = 0; x < n; x++){
-                String resultado = "";
-                for(int y = 0; y < n; y++){
-                    resultado = resultado + " " + matrizMagica[x][y];
-                }
-                System.out.println("" + resultado);
+        }
+
+        for (int x = 0; x < n; x++) {
+            String resultado = "";
+            for (int y = 0; y < n; y++) {
+                resultado = resultado + " " + matrizMagica[x][y];
             }
+            System.out.println("" + resultado);
+        }
     }
 }
