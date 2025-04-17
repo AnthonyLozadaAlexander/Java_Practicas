f7cba9220d2888b737031c47b527478a7f608377
zDefcon20 <antholozadarequelme@gmail.com>
2025-04-17 15:20:27 -0500

    feat(MatrizSimetrica): agregar lógica para la entrada de filas en la matriz

--- src/EjerciciosArrays/MatrizSimetrica.java
+++ src/EjerciciosArrays/MatrizSimetrica.java
@@ -1,7 +1,22 @@
 package EjerciciosArrays;
 
+import java.util.Scanner;
+
 public class MatrizSimetrica {
   public static void main(String[] args) {
-    
+    Scanner input = new Scanner(System.in);
+    int matriz[][], nFilas = 0, nColumnas = 0;
+    System.out.println("----------------------------");
+    System.out.println("         BIENVENIDO         ");
+    System.out.println("----------------------------");
+    do {
+      System.out.println("Ingrese el numero de filas");
+      System.out.print("-> ");
+      if (!input.hasNextInt()) {
+        System.out.println("\nError: Debe Ingresar un numero entero\n");
+      } else {
+        nFilas = input.nextInt();
+      }
+    } while (nFilas <= 0 || nColumnas <= 0);
   }
 }
