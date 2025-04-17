839592b1b0c83fa413d0cc5f853be4377bb521fb
zDefcon20 <antholozadarequelme@gmail.com>
2025-04-17 00:34:07 -0500

    feat(Matrices): agregar clase Matrices con estructura básica y matriz de ejemplo

---
+++ src/EjerciciosArrays/Matrices.java
@@ -0,0 +1,19 @@
+package EjerciciosArrays;
+
+public class Matrices {
+    public static void main(String[] args) {
+        int matriz[][] = {{1,2,3},
+                          {4,5,6},
+                          {7,8,9}};
+
+        for(int i = 0; i < 3; i++) // filas
+        {
+            for(int j = 0; j < 3; j++) // columnas
+            {
+
+            }
+        }
+
+
+    }
+}
