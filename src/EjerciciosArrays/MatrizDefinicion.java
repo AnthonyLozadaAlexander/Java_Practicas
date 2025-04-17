31e54c8594400fc58ca31ffed5854fff10b392f1
zDefcon20 <antholozadarequelme@gmail.com>
2025-04-17 00:55:47 -0500

    refactor(MatrizDefinicion): mejorar la lógica de entrada y la inicialización de variables en la clase MatrizDefinicion

--- src/EjerciciosArrays/MatrizDefinicion.java
+++ src/EjerciciosArrays/MatrizDefinicion.java
@@ -3,21 +3,47 @@ package EjerciciosArrays;
 import java.util.Scanner;
 
 public class MatrizDefinicion {
-    public static void main(String[] args) {
-      Scanner input = new Scanner(System.in);
-      int matriz[][] , nFilas, nColumnas;
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    int matriz[][], nFilas = 0, nColumnas = 0;
 
-      System.out.println("--------------------------------------------");
-      System.out.println("                 BIENVENIDO                 ");
-      System.out.println("--------------------------------------------");
-      do{
+    System.out.println("--------------------------------------------");
+    System.out.println("                 BIENVENIDO                 ");
+    System.out.println("--------------------------------------------");
+    do {
       System.out.println("Ingrese el numero de filas de la matriz");
       System.out.print("-> ");
-      nFilas = input.nextInt();
+      if (!input.hasNextInt()) 
+      {
+        System.out.println("\nError: Debe ingresar un numero entero");
+        System.out.println("Por favor, vuelva a intentarlo.\n");
+        input.next(); // Limpiar el buffer
+        continue; // Volver a pedir el número de filas
+      }
+      else{
+        
+        nFilas = input.nextInt();
 
-      System.out.println("Ingrese el numero de columnas de la matriz");
-      System.out.print("-> ");
-      nColumnas = input.nextInt();
-      }while(nFilas <= 0 || nColumnas <= 0);
-    }
+        System.out.println("Ingrese el numero de columnas de la matriz");
+        System.out.print("-> ");
+        if (!input.hasNextInt()) {
+          System.out.println("\nError: Debe ingresar un numero entero");
+          System.out.println("Por favor, vuelva a intentarlo.\n");
+          input.next(); // Limpiar el buffer
+          continue; // Volver a pedir el número de filas
+        } else {
+          nColumnas = input.nextInt();
+          if (nFilas <= 0 || nColumnas <= 0) {
+            System.out.println("\nError: El numero de filas y columnas debe ser mayor a 0");
+            System.out.println("Por favor, vuelva a intentarlo.\n");
+          } else {
+            System.out.println("\nDatos De La Matriz");
+            System.out.println("Filas[" + nFilas + "]");
+            System.out.println("Columnas[" + nColumnas + "]");
+          }
+        }
+
+      }
+    } while (nFilas <= 0 || nColumnas <= 0);
+  }
 }
