445cafd4098f6870d52959810d83f63ff917a7ff
zDefcon20 <antholozadarequelme@gmail.com>
2025-04-17 15:27:07 -0500

    feat(MatrizSimetrica): mejorar la validación de entrada para filas y columnas

--- src/EjerciciosArrays/MatrizSimetrica.java
+++ src/EjerciciosArrays/MatrizSimetrica.java
@@ -14,9 +14,36 @@ public class MatrizSimetrica {
       System.out.print("-> ");
       if (!input.hasNextInt()) {
         System.out.println("\nError: Debe Ingresar un numero entero\n");
+        input.next();
+        continue;
       } else {
         nFilas = input.nextInt();
+        if (nFilas <= 0) {
+          System.out.println("\nError: Debe Ingresar un numero entero mayor a 0\n");
+          continue;
+        } else {
+          System.out.println("Filas Ingresadas = [" + nFilas + "]");
+          System.out.println("----------------------------");
+          System.out.println("Ingrese el numero de Columnas");
+          System.out.print("-> ");
+          if (!input.hasNextInt()) {
+            System.out.println("\nError: Debe Ingresar un numero entero\n");
+            input.next();
+            continue;
+          } else {
+            nColumnas = input.nextInt();
+            if (nColumnas <= 0) {
+              System.out.println("Error: Debe Ingresar un numero entero mayor a 0\n");
+              continue;
+            } else {
+              System.out.println("Columnas Ingresadas = [" + nColumnas + "]");
+              System.out.println("----------------------------");
+            }
+          }
+        }
       }
     } while (nFilas <= 0 || nColumnas <= 0);
+    matriz = new int[nFilas][nColumnas];
+
   }
 }
