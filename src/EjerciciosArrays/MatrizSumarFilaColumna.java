08cf403a30829b3b61cce146def60a4a07ad907a
zDefcon20 <antholozadarequelme@gmail.com>
2025-04-21 00:41:52 -0500

    feat(MatrizSumarFilaColumna): agregar clase para sumar filas y columnas de una matriz

---
+++ src/EjerciciosArrays/MatrizSumarFilaColumna.java
@@ -0,0 +1,9 @@
+package EjerciciosArrays;
+import java.util.Scanner;
+
+public class MatrizSumarFilaColumna {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+    }
+}
