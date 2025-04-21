1fb1ddfabb9eb1e82e92f62753b10a47e52e1a53
zDefcon20 <antholozadarequelme@gmail.com>
2025-04-21 00:45:33 -0500

    feat(MatrizSumarFilaColumna): agregar validación para la entrada de filas y columnas

--- src/EjerciciosArrays/MatrizSumarFilaColumna.java
+++ src/EjerciciosArrays/MatrizSumarFilaColumna.java
@@ -4,6 +4,45 @@ import java.util.Scanner;
 public class MatrizSumarFilaColumna {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        int Matriz[][], nFilas = 0, nColumnas = 0;
+
+        System.out.println("----------------------------");
+        System.out.println("         BIENVENIDO         ");
+        System.out.println("----------------------------");
+        do {
+            System.out.println("Ingrese el numero de filas");
+            System.out.print("-> ");
+            if (!input.hasNextInt()) {
+                System.out.println("\nError: Debe Ingresar un numero entero\n");
+                input.next();
+                continue;
+            } else {
+                nFilas = input.nextInt();
+                if (nFilas <= 0) {
+                    System.out.println("\nError: Debe Ingresar un numero entero mayor a 0\n");
+                } else {
+                    System.out.println("Filas Ingresadas = [" + nFilas + "]");
+                    System.out.println("---------------------------------------------------------");
+                    System.out.println("Ingrese el numero de Columnas");
+                    System.out.print("-> ");
+                    if (!input.hasNextInt()) {
+                        System.out.println("\nError: Debe Ingresar un numero entero\n");
+                        input.next();
+                        continue;
+                    } else {
+                        nColumnas = input.nextInt();
+                        if (nColumnas <= 0) {
+                            System.out.println("Error: Debe Ingresar un numero entero mayor a 0\n");
+                        } else {
+                            System.out.println("Columnas Ingresadas = [" + nColumnas + "]");
+                            System.out.println("---------------------------------------------------------");
+                        }
+                    }
+                }
+            }
+        } while (nFilas <= 0 || nColumnas <= 0);
+
+        Matriz = new int[nFilas][nColumnas];
 
     }
 }
