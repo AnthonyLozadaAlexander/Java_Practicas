9d950ec6412e11f8f5334a629fb81fbbe1afbefc
zDefcon20 <antholozadarequelme@gmail.com>
2025-04-25 15:13:12 -0500

    feat(TrianguloHeron): agregar clase para calcular el área de un triángulo usando el método de Herón

---
+++ src/CalcularFiguras/TrianguloHeron.java
@@ -0,0 +1,9 @@
+package CalcularFiguras;
+
+import java.util.Scanner;
+
+public class TrianguloHeron {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+    }
+}
