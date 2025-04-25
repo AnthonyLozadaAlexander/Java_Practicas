f40a0b341c858d2960b916df73c2f1d0cb9c9c42
zDefcon20 <antholozadarequelme@gmail.com>
2025-04-25 15:28:43 -0500

    feat(MetodoHeron): agregar métodos para calcular el semiperímetro y el área de un triángulo

--- src/CalcularFiguras/MetodoHeron.java
+++ src/CalcularFiguras/MetodoHeron.java
@@ -1,5 +1,13 @@
 package CalcularFiguras;
 
 public class MetodoHeron {
-
+    public static float semiArea(float a, float b, float c) {
+        float S = (a + b + c) / 2;
+        return S;
+    }
+    public static float areaTriangulo(float a, float b, float c){
+        float S = (a + b + c) / 2;
+        float Area = (float) Math.sqrt(S*(S - a)*(S-b)*(S-c));
+        return Area;
+    }
 }
