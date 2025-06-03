dc8ed821c404c9503b8fa8e071024acbea91cb13
zDefcon20 <antholozadarequelme@gmail.com>
2025-06-03 01:22:09 -0500

    feat(Ejercicio_Array8): agregar funcionalidad para ingresar y mostrar elementos de una frase

--- src/Ejercicios/Ejercicio_Array8.java
+++ src/Ejercicios/Ejercicio_Array8.java
@@ -1,7 +1,27 @@
 package Ejercicios;
 
+import java.util.Scanner;
+
 public class Ejercicio_Array8 {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Ingrese una frase");
+        System.out.print("-> "); //ABCD
+        String frase = input.nextLine();
+        char[] array = new char[frase.length()];
+        for (int i = 0; i < array.length ; i++) {
+            array[i] = frase.charAt(i); // A = [0], B = [1], C = [2], D = [3]
+        }
+        System.out.println("Elementos de la frase");
+        String coma = ", ";
+        for (int i = 0; i < array.length; i++) {
 
+            if(i == array.length - 1){
+                System.out.print(array[i]); // No imprimir coma al final
+            }else{
+                System.out.print(array[i] + coma); // Imprimir coma entre elementos
+            }
+        }
+        input.close();
     }
 }
