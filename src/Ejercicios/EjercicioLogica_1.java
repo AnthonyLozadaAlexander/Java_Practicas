2a948646d4dd6a1f232fe93d5b273b0724988f44
zDefcon20 <antholozadarequelme@gmail.com>
2025-06-27 02:41:00 -0500

    refactor(EjercicioLogica_1): cambiar método ordenarBubbleSort a estático y mejorar formato de salida

--- src/Ejercicios/EjercicioLogica_1.java
+++ src/Ejercicios/EjercicioLogica_1.java
@@ -3,12 +3,12 @@ package Ejercicios;
 import java.util.Scanner;
 
 public class EjercicioLogica_1 {
-  public void ordenarBubbleSort(int[] array){
+  public static void ordenarBubbleSort(int[] array) {
     int aux;
     boolean cambios = false;
     int pasadas = 0;
 
-    while(true){
+    while (true) {
       cambios = false;
       for (int i = 0; i < array.length; i++) {
         if (array[i] < array[i - 1]) {
@@ -24,19 +24,34 @@ public class EjercicioLogica_1 {
     }
 
   }
+
   public static void main(String[] args) {
     Scanner input = new Scanner(System.in);
+    System.out.println("--------------------------------------------------");
+    System.out.println("Ordenamiento de un array con el método Bubble Sort");
+    System.out.println("--------------------------------------------------");
     System.out.println("Ingrese la cantidad de elementos del array");
     System.out.print("-> ");
     int n = input.nextInt();
     int[] arreglo = new int[n];
+    System.out.println("--------------------------------------------------");
     System.out.println("Ingrese los elementos del array");
     for (int i = 0; i < arreglo.length; i++) {
-      System.out.print("Elemento["+(i + 1) + "]: ");
+      System.out.print("Elemento[" + (i + 1) + "]: ");
       arreglo[i] = input.nextInt();
     }
+    System.out.println("--------------------------------------------------");
+    System.out.println("Array Actual: ");
+    for (int i = 0; i < arreglo.length; i++) {
+      System.out.println("Elemento[" + (i + 1) + "]: " + arreglo[i]);
+    }
+    System.out.println("--------------------------------------------------");
+    ordenarBubbleSort(arreglo); // función que ordena el array
+    System.out.println("Array Ordenado: ");
+    for (int i = 0; i < arreglo.length; i++) {
+      System.out.println("Elemento[" + (i + 1) + "]: " + arreglo[i]);
+    }
+    System.out.println("--------------------------------------------------");
 
-    ordenarBubbleSort(arreglo);
-    
   }
 }
