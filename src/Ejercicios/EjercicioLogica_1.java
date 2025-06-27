0f017b97818befab8fa364d3719ee6273486b0d6
zDefcon20 <antholozadarequelme@gmail.com>
2025-06-27 02:25:24 -0500

    feat(EjercicioLogica_1): agregar clase EjercicioLogica_1 con método main y Scanner

---
+++ src/Ejercicios/EjercicioLogica_1.java
@@ -0,0 +1,10 @@
+package Ejercicios;
+
+import java.util.Scanner;
+
+public class EjercicioLogica_1 {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    
+  }
+}
