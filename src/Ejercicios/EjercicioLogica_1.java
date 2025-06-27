97beaddcd023566f1eac84b53874e8699b5e0e85
zDefcon20 <antholozadarequelme@gmail.com>
2025-06-27 02:34:17 -0500

    feat(EjercicioLogica_1): implementar método ordenarBubbleSort y agregar entrada de usuario para el array

--- src/Ejercicios/EjercicioLogica_1.java
+++ src/Ejercicios/EjercicioLogica_1.java
@@ -3,8 +3,35 @@ package Ejercicios;
 import java.util.Scanner;
 
 public class EjercicioLogica_1 {
+  public int ordenarBubbleSort(int[] array){
+    int aux;
+    boolean cambios = false;
+    int pasadas = 0;
+
+    while(true){
+      cambios = false;
+      for (int i = 0; i < array.length; i++) {
+        if (array[i] < array[i - 1]) {
+          aux = array[i];
+          array[i] = array[i - 1];
+          array[i - 1] = aux;
+          cambios = true;
+        }
+      }
+      pasadas++;
+      if (!cambios) {
+        break;
+      }
+    }
+    return pasadas;
+  }
   public static void main(String[] args) {
     Scanner input = new Scanner(System.in);
+    System.out.println("Ingrese la cantidad de elementos del array");
+    System.out.print("-> ");
+    int n = input.nextInt();
+    int[] array = new int[n];
+    System.out.println("Ingrese los elementos del array");
     
   }
 }
